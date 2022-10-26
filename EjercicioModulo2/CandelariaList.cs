b74a97f351850ebae9e881d0a13e9eec37467634
candelariaSobol <candee_sobol@hotmail.com>
2022-10-26 19:12:06 -0300

    Add project files.

---
+++ EjercicioModulo2/CandelariaList.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioModulo2
+{
+    internal class CandelariaList
+    {
+
+        public int[] candelariaList = { 10, 15, 12, 0, 0 };
+
+        int capacidad = 5;         // Capacidad maxima del array
+        int cantidad = 3;          // Número real de numeros guardados
+        int i;
+
+        public CandelariaList()
+        {
+        }
+
+
+        public void agregarElemento()
+        {
+            int numero;
+           
+
+            Console.WriteLine("Ingrese el numero que desea ingresar"); //pedimos al usuario que agregue un numero
+            numero = Convert.ToInt32(Console.ReadLine());              //Almacenamos le numero en la variable numero
+            Console.WriteLine("Añadiendo el numero {0} al final", numero); //mensaje con el numero que se va a agregar
+            if (cantidad < capacidad)                                       //Verificamos si el array tiene lugar para el numero nuevo
+            {
+                
+                candelariaList[cantidad] = numero;
+                cantidad++;
+                capacidad = candelariaList.Length;
+            }
+            else if (candelariaList.Length == cantidad)
+            {
+                Array.Resize(ref this.candelariaList, this.candelariaList.Length * 2);
+
+                candelariaList[cantidad] = numero;
+                cantidad++;
+                capacidad = candelariaList.Length * 2;
+
+
+            }
+
+            foreach (int value in candelariaList)
+            {
+                Console.WriteLine(value);
+            }
+
+            //     // Y volvemos a mostrar el array
+            //    for (i = 0; i < cantidad; i++)
+            //    Console.Write("{0} ", candelariaList[i]);
+            //    Console.WriteLine();
+            //
+        }
+
+
+        public void agregarElementoPosicion()
+        {
+            int numero;
+            int posicionInsertar;
+
+            for (i = 0; i < cantidad; i++)
+                Console.Write("{0} ", candelariaList[i]);
+            Console.WriteLine();
+
+            Console.WriteLine("Ingrese el numero que desea ingresar"); 
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese en que posicion lo quiere ingresar");
+            posicionInsertar = (Convert.ToInt32(Console.ReadLine()) - 1);
+            
+
+            if (cantidad < capacidad)
+            {
+                Console.WriteLine("Añadiendo el numero {0} en la posicion {1}", numero, posicionInsertar + 1);
+                for (i = cantidad; i > posicionInsertar; i--) // los que queden atras tienen que moverse a la derecha  para dejarle hueco
+                    candelariaList[i] = candelariaList[i - 1];
+                //candelariaList[posicionInsertar - 1] = numero;
+                candelariaList[posicionInsertar] = numero;
+                cantidad++;
+                capacidad = candelariaList.Length;
+            }
+
+            else if (candelariaList.Length == cantidad)
+            {
+                Array.Resize(ref this.candelariaList, this.candelariaList.Length * 2);
+
+                candelariaList[cantidad] = numero;
+                cantidad++;
+                capacidad = candelariaList.Length * 2;
+
+
+            }
+
+            // Y volvemos a mostrar el array
+            for (i = 0; i < cantidad; i++)
+                Console.Write("{0} ", candelariaList[i]);
+            Console.WriteLine();
+        }
+
+
+        public void eliminarNumero()
+        {
+            int i;
+            int posicionEliminar;
+
+            //mostramos el array
+            for (i = 0; i < cantidad; i++)
+                Console.Write("{0} ", candelariaList[i]);
+            Console.WriteLine();
+
+            Console.WriteLine("Ingrese la posicion del numero que desea eliminar");
+            posicionEliminar = (Convert.ToInt32(Console.ReadLine()) - 1 ); //como el array comienza en 0 le restamos 1
+
+            // Borramos el numero
+            Console.WriteLine("Borrando el {0} numero", posicionEliminar);
+            for (i = posicionEliminar; i < cantidad; i++) //para eliminar un numero los que estaban a continuacion tienen que moversea la izquierda para que no quede un lugar vacio
+                candelariaList[i] = candelariaList[i + 1];
+            cantidad--;
+
+            //if(candelariaList.Length > cantidad)
+            //{
+            //    Array.Resize(ref this.candelariaList, this.candelariaList.Length / 2);
+            //}
+
+            // Y volvemos a mostrar el array
+            for (i = 0; i < cantidad; i++)
+                Console.Write("{0} ", candelariaList[i]);
+            Console.WriteLine();
+        }
+
+
+        public void buscarElemento()
+        {
+            int i;
+            int numero;
+
+            //mostramos el array para acordarnos de los datos
+            for (i = 0; i < cantidad; i++)
+            Console.Write("{0} ", candelariaList[i]);
+            Console.WriteLine();
+
+            Console.WriteLine("Ingrese el numero que desea buscar");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            // Buscamos el numero ingresado
+            for (i = 0; i < cantidad; i++)
+            {
+                if (candelariaList[i] == numero)
+                    Console.WriteLine("{0} encontrado en la posición {1} ", numero, i + 1);
+            }
+            //Error con el mensaje del numero no se encuentra
+
+            if (candelariaList[i] != numero)
+            {
+                Console.Write("El numero ingresado no se encuentra");
+            }
+
+        }
+
+        public void buscarPosicion()
+        {
+           
+            int posicion;
+            
+            
+
+            Console.WriteLine("Ingrese en que posicion desea saber que numero se encuentra");
+            posicion = (Convert.ToInt32(Console.ReadLine()) -1 );
+
+            Console.WriteLine("En la posicion {0} se encuentra el numero " + candelariaList.GetValue(posicion) , posicion + 1);
+
+
+        }
+
+        public void ordenarMayor()
+        {
+            
+            //mostramos el array para acordarnos de los datos
+            for (i = 0; i < cantidad; i++)
+                Console.Write("{0} ", candelariaList[i]);
+            Console.WriteLine();
+
+            //ordenamos el array
+            Array.Sort(candelariaList);
+
+            for (i = 0; i < cantidad; i++)
+                Console.Write("{0} ", candelariaList[i]);
+            Console.WriteLine();
+
+        }
+
+    }
+}
---
+++ EjercicioModulo2/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioModulo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+          CandelariaList  lista = new CandelariaList();
+
+            bool salir = false;
+
+            while (!salir)
+            {
+
+                try
+                {
+
+                    Console.WriteLine("1. Ingrese 1 para agregar un elemento al final de la lista");
+                    Console.WriteLine("2. Ingrese 2 para agregar un elemento en una posicion determinada de la lista");
+                    Console.WriteLine("3. Ingrese 3 para eliminar un elemento");
+                    Console.WriteLine("4. Ingrese 4 para buscar un elemento en la lista");
+                    Console.WriteLine("5. Ingrese 5 para obtener el elemento de la posicion indicada");
+                    Console.WriteLine("6. Ingrese 6 para ordenar el array de mayor a menor");
+                    Console.WriteLine("7. Ingrese 7 para salir de la aplicacion");
+                    Console.WriteLine("Elige una de las opciones");
+                    int opcion = Convert.ToInt32(Console.ReadLine());
+
+                    switch (opcion)
+                    {
+                        case 1:
+                            Console.WriteLine("Has elegido la opción 1");
+                            lista.agregarElemento();
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Has elegido la opción 2");
+                            lista.agregarElementoPosicion();
+                            break;
+
+                        case 3:
+                            Console.WriteLine("Has elegido la opción 3");
+                            lista.eliminarNumero();
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Has elegido la opción 4");
+                            lista.buscarElemento();
+                            break;
+
+                        case 5:
+                            Console.WriteLine("Has elegido la opción 5");
+                            lista.buscarPosicion();
+                            break;
+
+                        case 6:
+                            Console.WriteLine("has elegido ordenar el array de mayor a menor");
+                            lista.ordenarMayor();
+                            break;
+                        case 7:
+                            Console.WriteLine("Has elegido salir de la aplicación");
+                            salir = true;
+                            break;
+                        default:
+                            Console.WriteLine("Elige una opcion entre 1 y 7");
+                            break;
+                    }
+
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+           
+
+        }
+    
+
+
+    }
+}
+
